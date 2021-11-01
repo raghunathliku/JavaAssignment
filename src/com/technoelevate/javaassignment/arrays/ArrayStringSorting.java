0d61e4151185270894fc39c2352955f99a713aab
raghunathliku <raghunathdas115@gmail.com>
2021-11-01 10:10:53 +0530

    modified assingment 2file added

--- src/com/technoelevate/javaassignment/arrays/ArrayStringSorting.java
+++ src/com/technoelevate/javaassignment/arrays/ArrayStringSorting.java
@@ -1,43 +1,27 @@
 package com.technoelevate.javaassignment.arrays;
 
+
+import java.util.Arrays;  
+import java.util.Scanner;
+
 public class ArrayStringSorting {
-	public static void sortStringArray(String[] strArr, int sizeOfArr) {
-		String temp;
-		for (int i=0; i<sizeOfArr;i++) {
-			for (int j=1; j<sizeOfArr; j++) {
-				if (strArr[j-1].compareTo(strArr[j])>0) {
-					temp = strArr[j-1];
-					strArr[j-1]=strArr[j];
-					strArr[j]=temp;
-				}
-//				for (String str: strArr) {
-//					System.out.print("Iteration "+(i+1)+" : "+str+" ");
-//					System.out.println();
-//				}
-			}
-		}
-		System.out.print("Sorted String array :");
-		for (String str: strArr) {
-			System.out.print(str+" ");
-		}
-		//sortStringArray(strArr,sizeOfArr);
-	}
-
-
-	public static void main(String[] args) {
-		
-			String[] strArr = {"Java", "Python", "PHP", "C#", "C Programming", "C++"};
-			int sizeOfArr = strArr.length;
-			System.out.print("Original String array : ");
-			for (String str: strArr) {
-				System.out.print(str+" ");
-			}
-			System.out.println();
-			sortStringArray(strArr,sizeOfArr);
-
-		}
-		
-
 	
+
+	/*4. Expected Output: 
+		   Original string array : [Java, Python, PHP, C#, C Programming, C++]                                           
+		   Sorted string array : [C Programming, C#, C++, Java, PHP, Python] 
+	*/
+	  
+	   public static void main(String args[])   
+	   {  
+
+
+	      String str[]= {"Java","Phthon","PHP","C#","C Programming","C++"}; 
+
+
+	      Arrays.sort(str); 
+
+	      System.out.println(Arrays.toString(str)); 
+	   }  
 
 }
--- src/com/technoelevate/javaassignment/arrays/RangeOfNumber.java
+++ src/com/technoelevate/javaassignment/arrays/RangeOfNumber.java
@@ -5,31 +5,30 @@ package com.technoelevate.javaassignment.arrays;
 	Sum all the numbers that can be divided with both 3 and 5.
 	for those numbers that met the above conditions, print out the number
 	break out of the loop once you find 5 numbers that met the above condition
-	After breaking out of the loop peint the sum of the numbers that met the above conditions.*/
+	After breaking out of the loop print the sum of the numbers that met the above conditions.*/
 	public class RangeOfNumber {
 	public static void main(String[] args) {
 		int sum=0;	
-		int fiveSum=0;
-		for(int number=1;number<=1000;number++) {
+		int count=0;
+		for(int i=1;i<=1000;i++) {
 
-		if(number%3==0 || number%5==0) {
-			sum=number+sum;
+		if(i%3==0 && i%5==0) {
+			count++;
+			System.out.println("First digit is "+i);
 		}
+		sum=sum+i;
 		
 
+		
+		if(count==5) {
+			System.out.println("sum of first five digit is "+sum);
+			break;
 		}
-		/*
-		 * for(int j=1;j<=1000;j++) { int count=0; if(count<=5) { if(j%3==0 || j%5==0) {
-		 * 
-		 * fiveSum=j+fiveSum; count++; } }
-		 * 
-		 * }
-		 */
-		System.out.println("Sum of the numbers that divided by 3 and 5 :"+sum);
-		/*
-		 * System.out.println("Sum of first five number that divided by 3 and 5 :"
-		 * +fiveSum);
-		 */
+	}
+		  
+		  
+	}		 
+		
 	}
 
-}
+		  
