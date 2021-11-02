9d32a14c078ed8b79d801dbb12ba840c201d6b40
raghunathliku <raghunathdas115@gmail.com>
2021-11-02 09:21:24 +0530

    modified

--- 01-11-2021/ReverseString.java
+++ 01-11-2021/ReverseString.java
@@ -2,7 +2,7 @@ package techno.java.assign;
 
 public class ReverseString {
 public static void main(String[] args) {
-	String string="My Name is Sandesh";
+	String string="My Name is Raghunath";
 	String array[] = string.split(" ");
 	for(int i=array.length-1;i>=0;i--){
 		System.out.print(array[i]+" ");
